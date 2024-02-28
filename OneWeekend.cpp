de14ac1655317883078b172533edbc97dd3824d5
unknown <antenna.tombow@gmail.com>
2024-02-28 16:28:35 -0500

    Add texture mapping

--- OneWeekend.cpp
+++ OneWeekend.cpp
@@ -9,13 +9,13 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
+#include "texture.h"
 
-
-int main() {
+void random_spheres() {
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, material_ground));
+    auto checker = make_shared<checker_texture>(0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
@@ -61,20 +61,76 @@ int main() {
 
     camera cam;
 
-    cam.aspect_ratio        = 16.0 / 9.0;
-    cam.image_width         = 400;
-    cam.samples_per_pixel   = 100;
-    cam.max_depth           = 50;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
 
-    cam.vfov        = 20;
-    cam.lookfrom    = point3(13, 2, 3);
-    cam.lookat      = point3(0, 0, 0);
-    cam.vup         = vec3(0, 1, 0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
     cam.focus_dist = 10.0;
 
     cam.render(world);
+}
+
+void two_spheres() {
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(0.8, color(.2, .3, .1), color(.9, .9, .9));
+
+    world.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
+void earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(12, 0, 0);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
+
+int main() {
+    switch (3) {
+        case 1: random_spheres(); break;
+        case 2: two_spheres();    break;
+        case 3: earth();          break;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
