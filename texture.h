1549210e4f80b31f56fd67c089a824f233f91116
unknown <antenna.tombow@gmail.com>
2024-03-22 18:01:12 -0400

    Implement Perlin noise

--- texture.h
+++ texture.h
@@ -4,6 +4,7 @@
 
 #include "rtweekend.h"
 #include "rtw_stb_image.h"
+#include "perlin.h"
 
 class texture {
 public:
@@ -76,6 +77,22 @@ public:
 
 private:
 	rtw_image image;
+};
+
+class noise_texture : public texture {
+public:
+	noise_texture() {}
+
+	noise_texture(double sc) : scale(sc) {}
+
+	color value(double u, double v, const point3& p) const override {
+		auto s = scale * p;
+		return color(1, 1, 1) * 0.5 * (1 + sin(s.z() + 10 * noise.turb(s)));
+	}
+
+private:
+	perlin noise;
+	double scale;
 };
 
 
