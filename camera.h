988e4d1d320dd907f0fb54666cec477ad05a2f3c
unknown <antenna.tombow@gmail.com>
2024-04-02 19:59:55 -0400

    Implement quads and lights

--- camera.h
+++ camera.h
@@ -17,6 +17,7 @@ public:
 	int image_width			= 100;	// Rendered image width in pixel count
 	int samples_per_pixel	= 10;	// Count of random samples for each pixel
 	int max_depth			= 10;	// Maximum number of ray bounces into scene
+	color background;				// Scene background color
 
 	double vfov = 90;					// Vertical view angle in degrees (field of view)
 	point3 lookfrom = point3(0, 0, -1);	// Point camera is looking from
@@ -47,7 +48,7 @@ public:
 	}
 
 private:
-	int image_height;		// Rendered iamge height
+	int image_height;		// Rendered image height
 	point3 center;			// Camera center
 	point3 pixel00_loc;		// Location of pixel 0, 0
 	vec3 pixel_delta_u;		// Offset to pixel to the right
@@ -129,17 +130,20 @@ private:
 			return color(0, 0, 0);
 		}
 
-		if (world.hit(r, interval(0.001, infinity), rec)) {
-			ray scattered;
-			color attenuation;
-			if (rec.mat->scatter(r, rec, attenuation, scattered))
-				return attenuation * ray_color(scattered, depth - 1, world);
-			return color(0, 0, 0);
-		}
+		// If ray hits nothing, return the background color.
+		if (!world.hit(r, interval(0.001, infinity), rec))
+			return background;
+		
+		ray scattered;
+		color attenuation;
+		color color_from_emission = rec.mat->emitted(rec.u, rec.v, rec.p);
 
-		vec3 unit_direction = unit_vector(r.direction());
-		auto a = 0.5 * (unit_direction.y() + 1.0);
-		return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+		if (!rec.mat->scatter(r, rec, attenuation, scattered))
+			return color_from_emission;
+
+		color color_from_scatter = attenuation * ray_color(scattered, depth - 1, world);
+		
+		return color_from_emission + color_from_scatter;
 	}
 };
 
