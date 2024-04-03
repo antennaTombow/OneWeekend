f3892c9173671b93dfdd9c4ca5738ec06d61eade
unknown <antenna.tombow@gmail.com>
2024-04-03 00:38:00 -0400

    Add instance translation and rotation

--- aabb.h
+++ aabb.h
@@ -65,4 +65,12 @@ public:
 	}
 
 };
+
+aabb operator+(const aabb& bbox, const vec3& offset) {
+	return aabb(bbox.x + offset.x(), bbox.y + offset.y(), bbox.z + offset.z());
+}
+
+aabb operator+(const vec3& offset, const aabb& bbox) {
+	return bbox + offset;
+}
 #endif // !AABB_H
